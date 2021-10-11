b6a1317829a96c06a640bf1d34242d3b27f34c43
nikodem.wolczuk <nikodem.wolczuk@ibl.waw.pl>
2021-10-11 15:43:00 +0200

    add regex to searching

--- searcher/views.py
+++ searcher/views.py
@@ -7,7 +7,11 @@ from .models import Descriptors
 def home(request):
     if request.method == 'POST':
         search_query = request.POST['search_query']
-        results = Descriptors.objects.filter(descriptor__contains=search_query.lower())
+        if search_query.startswith('any:'):
+            results = Descriptors.objects.filter(descriptor__contains=search_query[4:].lower())
+        else:
+            regex_pattern = '^' + search_query.capitalize() + '|^' + search_query.lower() + '|\$.' + search_query.lower() + '|\$.' + search_query.capitalize() + '| ' + search_query.capitalize() + '| ' + search_query.lower()
+            results = Descriptors.objects.filter(descriptor__regex=regex_pattern)
         return render(request, 'searcher/result.html', {'search_query': search_query, 'results': results})
     else: 
         return render(request, 'searcher/base.html')
